350eac897479389c4e5c5d34ec7f64119f274533
Jarrod Rogers <jtr190004@utdallas.edu>
2024-03-06 18:44:33 -0600

    Basic fileupload

--- Pages/FileUpload.cshtml.cs
+++ Pages/FileUpload.cshtml.cs
@@ -8,5 +8,31 @@ namespace PetEmotionsApp.Pages
         public void OnGet()
         {
         }
+        public async Task<IActionResult> OnPostUploadAsync()
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                await FileUpload.FormFile.CopyToAsync(memoryStream);
+
+                // Upload the file if less than 2 MB
+                if (memoryStream.Length < 2097152)
+                {
+                    var file = new FileUpload();
+                    {
+                        Content = memoryStream.ToArray();
+                    };
+
+                    _context.File.Add(file);
+
+                    await _context.SaveChangesAsync();
+                }
+                else
+                {
+                    ModelState.AddModelError("File", "The file is too large.");
+                }
+            }
+
+        return Page();
+        }
     }
 }
