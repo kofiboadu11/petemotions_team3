e4b32bcacd744caa6a3b0c28f0280709ef24f0c4
Jarrod Rogers <jtr_gamer@hotmail.com>
2024-04-12 16:09:52 -0500

    History reverted to main branch

--- Pages/History.cshtml.cs
+++ Pages/History.cshtml.cs
@@ -1,29 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
-using PetEmotionsApp.Data;
-using PetEmotionsApp.Models;
 
-namespace PetEmotionsApp.Pages_FileUploader
+namespace PetEmotionsApp.Pages;
+
+public class HistoryModel : PageModel
 {
-    public class HistoryModel : PageModel
+  //  private readonly ILogger<HistoryModel> _logger;
+
+
+    public void OnGet()
     {
-        private readonly PetEmotionsApp.Data.PetEmotionsAppContext _context;
 
-        public HistoryModel(PetEmotionsApp.Data.PetEmotionsAppContext context)
-        {
-            _context = context;
-        }
-
-        public IList<FileUpload> FileUpload { get;set; } = default!;
-
-        public async Task OnGetAsync()
-        {
-            FileUpload = await _context.FileUpload.ToListAsync();
-        }
     }
 }
