344865ab99cc0844683bc354c8c5fcb44178e7f6
nabihamomin <nabiha.jm@gmail.com>
2024-04-12 15:27:56 -0500

    History backend changes, not properly fetching JSON feed

--- Pages/History.cshtml.cs
+++ Pages/History.cshtml.cs
@@ -1,5 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Microsoft.Extensions.Logging; // Import ILogger
+
 
 namespace PetEmotionsApp.Pages;
 
@@ -7,9 +12,35 @@ public class HistoryModel : PageModel
 {
   //  private readonly ILogger<HistoryModel> _logger;
 
+  private readonly ILogger<HistoryModel> _logger; // Declare ILogger<T> field
 
-    public void OnGet()
+        public HistoryModel(ILogger<HistoryModel> logger) // Inject ILogger<T> via constructor injection
+        {
+            _logger = logger;
+        }
+
+
+  public class calendarEvent{
+    public string title {get; set;}
+    public DateTime start {get; set;}
+    public DateTime end {get; set;}
+    public string color {get; set;}
+  }
+
+
+    public IActionResult OnGetMyFeed()
     {
+      _logger.LogDebug("OnGetMyFeed method called.");
+      calendarEvent CalendarEvent = new calendarEvent();
+      CalendarEvent.title = "happy";
+      CalendarEvent.start = new DateTime(2024,4,11);
+      CalendarEvent.end = new DateTime(2024,4,12);
+      CalendarEvent.color = "#F78D7D";
 
+     // string json = JsonConvert.SerializeObject(CalendarEvent);
+
+      //return json;
+
+      return new JsonResult(new[] { CalendarEvent });
     }
 }
