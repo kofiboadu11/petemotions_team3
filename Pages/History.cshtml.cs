f8290c38253eff593025de101050b8799a85edfc
kofiboadu11 <kofiboadu0456@gmail.com>
2024-04-11 19:54:53 -0500

    history changes

--- Pages/History.cshtml.cs
+++ Pages/History.cshtml.cs
@@ -1,15 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using PetEmotionsApp.Data;
+using PetEmotionsApp.Models;
 
-namespace PetEmotionsApp.Pages;
-
-public class HistoryModel : PageModel
+namespace PetEmotionsApp.Pages_FileUploader
 {
-  //  private readonly ILogger<HistoryModel> _logger;
-
-
-    public void OnGet()
+    public class HistoryModel : PageModel
     {
+        private readonly PetEmotionsApp.Data.PetEmotionsAppContext _context;
 
+        public HistoryModel(PetEmotionsApp.Data.PetEmotionsAppContext context)
+        {
+            _context = context;
+        }
+
+        public IList<FileUpload> FileUpload { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            FileUpload = await _context.FileUpload.ToListAsync();
+        }
     }
 }
